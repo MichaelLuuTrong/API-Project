d51fc8dcd635f3c697c847d7a85352180b817619
michaelluutrong <michaelluutrong@gmail.com>
2023-05-10 14:54:31 -0700

    Phase 1 doublecheck

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
@@ -10,9 +13,5 @@ router.get("/api/csrf/restore", (req, res) => {
         'XSRF-Token': csrfToken
     });
 });
-
-const apiRouter = require('./api');
-
-router.use('/api', apiRouter);
 
 module.exports = router;
