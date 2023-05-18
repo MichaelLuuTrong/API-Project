31bc9e152b57338a4072ce622ef522d1167ef8c5
michaelluutrong <michaelluutrong@gmail.com>
2023-05-18 09:10:19 -0700

    Create a Spot Completed

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,7 +1,45 @@
 const express = require('express')
 const router = express.Router();
-const { Booking, Review, ReviewImage, Spot, SpotImage, User } = require('../../db/models')
+const { requireAuth } = require('../../utils/auth')
+const { Booking, Review, ReviewImage, Spot, SpotImage, User } = require('../../db/models');
+const { handleValidationErrors } = require('../../utils/validation');
+const { check } = require('express-validator');
 
+const validateSpot = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .withMessage('Street address is required'),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage('City is required'),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage('State is required'),
+    check('country')
+        .exists({ checkFalsy: true })
+        .withMessage('Country is required'),
+    check('lat')
+        .exists({ checkFalsy: true })
+        .isFloat({ min: -90, max: 90 })
+        .withMessage('Latitude is not valid'),
+    check('lng')
+        .exists({ checkFalsy: true })
+        .isFloat({ min: -180, max: 180 })
+        .withMessage('Longitude is not valid'),
+    check('name')
+        .exists({ checkFalsy: true })
+        .withMessage('Invalid name')
+        .isLength({ max: 50 })
+        .withMessage('Name must be less than 50 characters'),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage('Description is required'),
+    check('price')
+        .exists({ checkFalsy: true })
+        .isNumeric({ checkFalsy: true })
+        .withMessage('Price per day is required'),
+    handleValidationErrors
+];
 
 router.get('/', async (req, res) => {
     const allSpots = await Spot.findAll({
@@ -48,6 +86,25 @@ router.get('/', async (req, res) => {
 
 
     return res.json({ Spots })
+})
+
+router.post('/', validateSpot, requireAuth, async (req, res) => {
+    const { address, city, state, country, lat, lng, name, description, price, createdAt, updatedAt } = req.body;
+    const ownerId = req.user.dataValues.id;
+    const newSpot = await Spot.create({
+        ownerId,
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price
+    })
+    res.status(201)
+    return res.json(newSpot)
 })
 
 
