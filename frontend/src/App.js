616b6bb656d008fafe35c4634f1420ee42a33e8e
michaelluutrong <michaelluutrong@gmail.com>
2023-06-10 18:23:17 -0700

    added handling for weird urls

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,6 +27,7 @@ function App() {
         <Route exact path="/spots/current" component={ManageSpots} />
         <Route exact path="/spots/:spotId" component={SpotPage} />
         <Route exact path="/spots/:spotId/edit" component={UpdateSpot} />
+        <Route component={SpotsIndex} />
       </Switch>}
     </div>
   );
