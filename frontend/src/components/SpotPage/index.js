5e81a7e1a4ae0aeadcfc5bf2410c0f740466ba3c
michaelluutrong <michaelluutrong@gmail.com>
2023-06-11 12:37:41 -0700

    started work on individual spot pages

--- frontend/src/components/SpotPage/index.js
+++ frontend/src/components/SpotPage/index.js
@@ -6,6 +6,7 @@ import ReviewsForSpot from "../ReviewsForSpot";
 import { getReviewsThunk } from "../../store/reviews";
 import OpenModalButton from "../OpenModalButton"
 import ReviewFormModal from "../ReviewFormModal"
+import './SpotPage.css'
 
 
 
@@ -43,73 +44,82 @@ const SpotPage = () => {
                 <p>{spotObj.city}, {spotObj.state}, {spotObj.country}</p>
             </div>
             <div className='picturesDiv'>
-                <div>
-                    <img src={spotObj.SpotImages[0] ? spotObj.SpotImages[0].url : placeholderImage} alt='main spot preview' className='bigLeftPicture' />
+                <div className='leftPicturesDiv'>
+                    <img src={spotObj.SpotImages[0] ? spotObj.SpotImages[0].url : placeholderImage} alt='large spot previw' className='leftSpotImg' />
                 </div>
                 <div className='rightPicturesDiv'>
-                    <img src={spotObj.SpotImages[1] ? spotObj.SpotImages[0].url : placeholderImage} alt='additional spot previews' className='smallRightPicture' />
-                    <img src={spotObj.SpotImages[2] ? spotObj.SpotImages[0].url : placeholderImage} alt='additional spot previews' className='smallRightPicture' />
-                    <img src={spotObj.SpotImages[3] ? spotObj.SpotImages[0].url : placeholderImage} alt='additional spot previews' className='smallRightPicture' />
-                    <img src={spotObj.SpotImages[4] ? spotObj.SpotImages[0].url : placeholderImage} alt='additional spot previews' className='smallRightPicture' />
+                    <div className='rightPicturesTopDiv'>
+                        <img src={spotObj.SpotImages[1] ? spotObj.SpotImages[0].url : placeholderImage} alt='small spot preview' className='rightSpotImg' />
+                        <img src={spotObj.SpotImages[2] ? spotObj.SpotImages[0].url : placeholderImage} alt='small spot preview' className='rightSpotImg' />
+                    </div>
+                    <div className='rightPicturesBottomDiv'>
+                        <img src={spotObj.SpotImages[3] ? spotObj.SpotImages[0].url : placeholderImage} alt='small spot preview' className='rightSpotImg' />
+                        <img src={spotObj.SpotImages[4] ? spotObj.SpotImages[0].url : placeholderImage} alt='small spot preview' className='rightSpotImg' />
+                    </div>
                 </div>
             </div>
             <div className='bodySpotInfo'>
-                <div className='hostAndSpotInfo'>
-                    <h2>Hosted by {spotObj.Owner.firstName} {spotObj.Owner.lastName}</h2>
-                    <p>{spotObj.description}</p>
-                </div>
-                <div className='reservationDiv'>
-                    <div>
-                        <p>${spotObj.price} night</p>
+                <div className='hostAndSpotInfoAndReservationBubble'>
+                    <div className='ownerAndSpotInfo'>
+                        <h2>Hosted by {spotObj.Owner.firstName} {spotObj.Owner.lastName}</h2>
+                        <p>{spotObj.description}</p>
                     </div>
-                    {!spotObj.avgStarRating ?
-                        <div className='newSpot'>
-                            <i className="fa-solid fa-star"></i>
-                            <h3>New</h3>
-                        </div>
-                        :
-                        < div className='reviewedSpot'>
-                            <h3><i className="fa-solid fa-star"></i>
-                                <div>{spotObj.avgStarRating % 1 === 0 ? (spotObj.avgStarRating + '.0') : Number.parseFloat(spotObj.avgStarRating).toFixed(1)}</div>
-                                <h4>•</h4>
-                                {spotObj.numReviews === 1 &&
-                                    <div className='oneReview'>{spotObj.numReviews} review</div>
-                                }
-                                {spotObj.numReviews > 1 &&
-                                    <div className='multipleReviews'>{spotObj.numReviews} reviews</div>
-                                }
-                            </h3>
-                        </div>}
-                    <div>
-                        <button onClick={clickReserve} className='reservationButton'>Reserve</button>
-                    </div>
-                    <div className='reviewsDiv'>
-                        <div className='ratingNumber'>
-                            <i className="fa-solid fa-star"></i>
-                            <h4>{spotObj.avgStarRating === null ? 'New' : (spotObj.avgStarRating % 1 === 0 ? (spotObj.avgStarRating + '.0') : Number.parseFloat(spotObj.avgStarRating).toFixed(1))}</h4>
-                            <h4>{spotObj.avgStarRating !== null ? '•' : null}</h4>
-                            <h4>
-                                {spotObj.numReviews === 1 &&
-                                    <div className='oneReview'>{spotObj.numReviews} review</div>
-                                }
-                                {spotObj.numReviews > 1 &&
-                                    <div className='multipleReviews'>{spotObj.numReviews} reviews</div>
-                                }
-                            </h4>
-                            {user && (user.id !== spotObj.ownerId && !usersWithReview.includes(user.id)) &&
-                                <div className="reviewPostDiv">
-                                    <OpenModalButton
-                                        cName="reviewPostButton changeCursor"
-                                        modalComponent={<ReviewFormModal spotId={spotId} />}
-                                        buttonText="Post Your Review"
-                                    />
+                    <div className='reservationDiv'>
+                        <div className='reservationWithoutButton'>
+                            <div className='priceAndNight'>
+                                <h3>${spotObj.price} night</h3>
+                            </div>
+                            {!spotObj.avgStarRating ?
+                                <div className='newSpot'>
+                                    <div className='justStar'><i className="fa-solid fa-star"></i></div>
+                                    <h3>New</h3>
                                 </div>
+                                :
+                                < div className='reviewedSpot'>
+                                    <div className='justStar'><i className="fa-solid fa-star"></i></div>
+                                    <div className='justRating'>{spotObj.avgStarRating % 1 === 0 ? (spotObj.avgStarRating + '.0') : Number.parseFloat(spotObj.avgStarRating).toFixed(1)}</div>
+                                    <div className='justDot'>•</div>
+                                    {spotObj.numReviews === 1 &&
+                                        <div className='oneReview'>{spotObj.numReviews} review</div>
+                                    }
+                                    {spotObj.numReviews > 1 &&
+                                        <div className='multipleReviews'>{spotObj.numReviews} reviews</div>
+                                    }
+                                </div>}
+                        </div>
+                        <div className='justReserveButton'>
+                            <button onClick={clickReserve} className='reservationButton'>Reserve</button>
+                        </div >
+                    </div>
+
+                </div>
+
+                <div className='reviewsDiv'>
+                    <div className='ratingNumber'>
+                        <i className="fa-solid fa-star"></i>
+                        <h4>{spotObj.avgStarRating === null ? 'New' : (spotObj.avgStarRating % 1 === 0 ? (spotObj.avgStarRating + '.0') : Number.parseFloat(spotObj.avgStarRating).toFixed(1))}</h4>
+                        <h4>{spotObj.avgStarRating !== null ? '•' : null}</h4>
+                        <h4>
+                            {spotObj.numReviews === 1 &&
+                                <div className='oneReview'>{spotObj.numReviews} review</div>
                             }
-                            {user && (!spotObj.numReviews && (user.id !== spotObj.ownerId && !usersWithReview.includes(user.id))) && <p>Be the first to post a review!</p>}
-                        </div>
-                        <div className='eachReview'>
-                            <ReviewsForSpot spotId={spotId} />
-                        </div>
+                            {spotObj.numReviews > 1 &&
+                                <div className='multipleReviews'>{spotObj.numReviews} reviews</div>
+                            }
+                        </h4>
+                        {user && (user.id !== spotObj.ownerId && !usersWithReview.includes(user.id)) &&
+                            <div className="reviewPostDiv">
+                                <OpenModalButton
+                                    cName="reviewPostButton changeCursor"
+                                    modalComponent={<ReviewFormModal spotId={spotId} />}
+                                    buttonText="Post Your Review"
+                                />
+                            </div>
+                        }
+                        {user && (!spotObj.numReviews && (user.id !== spotObj.ownerId && !usersWithReview.includes(user.id))) && <p>Be the first to post a review!</p>}
+                    </div>
+                    <div className='eachReview'>
+                        <ReviewsForSpot spotId={spotId} />
                     </div>
                 </div>
             </div>
